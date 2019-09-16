c0e9804b32dd5dae60e3cd76fae3d0258ac44ef4
Charitarth <charitarth.chugh@gmail.com>
2019-09-16 15:22:32 -0400

    Fixed issue where non void value is not bieng able to be retuned

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 
 public class Main {
 
-    private static String output1 = "";
+
 
     public static void main(String[] args) {
         //Hard coding each value in binary
@@ -12,7 +15,7 @@ public class Main {
         hexvalue = hexvalue.toUpperCase();
         voidcheck(hexvalue);
         System.out.println("The original hex value was: " + hexvalue);
-        System.out.println("The binary value is: " + convert(hexvalue));
+        System.out.println("The binary value is "+ convert(hexvalue));
         System.exit(0);
 
     }
@@ -29,42 +32,45 @@ public class Main {
         }
     }
 
-    public static String convert(String hexval) {
-        String[] hexvsluesarray = new String[]{"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" };
+    @Contract(pure = true)
+    public static String convert(@NotNull String hexval) {
+        String[] hexvaluesarray = new String[]{"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" };
         char[] hexchar = hexval.toCharArray();
-        for (int i = 1; i <= hexchar.length; i++) {
+        String output1="";
+        int length =hexchar.length;
+        for (int i = 1; i < length; i++) {
             if (hexchar[i] == '0') {
-                 output1 = output1 + hexvsluesarray[0];
+                 output1 = output1 + hexvaluesarray[0];
             } else if (hexchar[i] == '1') {
-                 output1 = output1 + hexvsluesarray[1];
+                 output1 = output1 + hexvaluesarray[1];
             } else if (hexchar[i] == '2') {
-                 output1 = output1 + hexvsluesarray[2];
+                 output1 = output1 + hexvaluesarray[2];
             } else if (hexchar[i] == '3') {
-                 output1 = output1 + hexvsluesarray[3];
+                 output1 = output1 + hexvaluesarray[3];
             } else if (hexchar[i] == '4') {
-                 output1 = output1 + hexvsluesarray[4];
+                 output1 = output1 + hexvaluesarray[4];
             } else if (hexchar[i] == '5') {
-                 output1 = output1 + hexvsluesarray[5];
+                 output1 = output1 + hexvaluesarray[5];
             } else if (hexchar[i] == '6') {
-                 output1 = output1 + hexvsluesarray[6];
+                 output1 = output1 + hexvaluesarray[6];
             } else if (hexchar[i] == '7') {
-                 output1 = output1 + hexvsluesarray[7];
+                 output1 = output1 + hexvaluesarray[7];
             } else if (hexchar[i] == '8') {
-                 output1 = output1 + hexvsluesarray[8];
+                 output1 = output1 + hexvaluesarray[8];
             } else if (hexchar[i] == '9') {
-                 output1 = output1 + hexvsluesarray[9];
+                 output1 = output1 + hexvaluesarray[9];
             } else if (hexchar[i] == 'A') {
-                 output1 = output1 + hexvsluesarray[10];
+                 output1 = output1 + hexvaluesarray[10];
             } else if (hexchar[i] == 'B') {
-                 output1 = output1 + hexvsluesarray[11];
+                 output1 = output1 + hexvaluesarray[11];
             } else if (hexchar[i] == 'C') {
-                 output1 = output1 + hexvsluesarray[12];
+                 output1 = output1 + hexvaluesarray[12];
             } else if (hexchar[i] == 'D') {
-                 output1 = output1 + hexvsluesarray[13];
+                 output1 = output1 + hexvaluesarray[13];
             } else if (hexchar[i] == 'E') {
-                 output1 = output1 + hexvsluesarray[14];
+                 output1 = output1 + hexvaluesarray[14];
             } else if (hexchar[i] == 'F') {
-                 output1 = output1 + hexvsluesarray[15];
+                 output1 = output1 + hexvaluesarray[15];
             } else {
                 output1 = "Invalid characters found.Terminating program.";
 
